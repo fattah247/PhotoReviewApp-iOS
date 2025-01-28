345914c1baaca958c726690cf0dfc60455db5fd2
Muhammad A Fattah <fattahmuhammad17@gmail.com>
2025-01-28 13:10:53 +0700

    Initial Commit

---
+++ PhotoReviewApp/PhotoReviewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoReviewAppApp.swift
+//  PhotoReviewApp
+//
+//  Created by Muhammad Abdul Fattah on 28/01/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoReviewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoReviewAppTests/PhotoReviewAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoReviewAppTests.swift
+//  PhotoReviewAppTests
+//
+//  Created by Muhammad Abdul Fattah on 28/01/25.
+//
+
+import Testing
+
+struct PhotoReviewAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
